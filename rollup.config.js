dbb4608ce520ba526146b7cd8ce8b463a1ba5670
Amir <itsAmirAsghary@gmail.com>
2022-06-03 23:17:30 +0430

    added support for typescript

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import resolve from '@rollup/plugin-node-resolve';
 import babel from '@rollup/plugin-babel';
+import typescript from '@rollup/plugin-typescript';
 import { uglify } from 'rollup-plugin-uglify'
 
 export default {
@@ -10,6 +11,7 @@ export default {
     },
     plugins: [
         resolve(),
+        typescript({ tsconfig: './tsconfig.json' }),
         babel({ babelHelpers: 'bundled' }),
         process.env.NODE_ENV === 'production' && uglify(),
     ]
